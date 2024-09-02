8025f0644b13d8c58096da7b2d69dfe33cb171bc
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2024-09-02 15:44:25 -0400

    Cambio Final

---
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+
+    public class Tecnicos
+    {
+        [Key]
+        public int tecniCold { get; set; }
+        public string? nombre {get; set; }
+        public double sueldoHora { get; set;}
+    }  
+}
+
