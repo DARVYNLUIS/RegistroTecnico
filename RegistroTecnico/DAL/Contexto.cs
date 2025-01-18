b55adf4d1e4b13f9802a1d6379f581b403244e64
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-01-17 21:16:17 -0400

    Contexto creado.

---
+++ RegistroTecnico/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using RegistroTecnico.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace RegistroTecnico.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+}
