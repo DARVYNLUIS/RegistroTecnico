a1fc9bdb53d7f43822d6d4745f1725ea16e84f56
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-01-27 19:47:23 -0400

    Modelo agregado.

---
+++ RegistroTecnico/Models/Ciudad.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnico.Models
+{
+    public class Ciudad
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+
+        public string Nombre { get; set; } = null!;
+
+    }
+}
