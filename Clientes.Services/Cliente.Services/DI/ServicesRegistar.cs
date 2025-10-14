23d767a8b4c41006f29e7997d53962f4e2f7091d
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-10-13 20:08:58 -0400

    Services Registar.

---
+++ Clientes.Services/Cliente.Services/DI/ServicesRegistar.cs
@@ -0,0 +1,26 @@
+﻿using ClienteAPI.Abstractions;
+using Clientes.Data.DAL;
+using Clientes.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ClienteAPI.Services.DI
+{
+    public static class ServicesRegistrar
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqlConStr");
+
+            services.AddDbContextFactory<Contexto>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<IClientesService, ClientesService>();
+
+            return services;
+        }
+
+    }
+}
