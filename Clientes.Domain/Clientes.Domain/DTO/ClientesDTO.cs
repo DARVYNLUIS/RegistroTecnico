3d2ffac8183ec901602f3cc93540db12528534cf
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-10-13 20:07:20 -0400

    ClientesDTO Creado.

---
+++ Clientes.Domain/Clientes.Domain/DTO/ClientesDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Clientes.Domain.DTO
+{
+    public class ClientesDto
+    {
+        public int ClienteId { get; set; } 
+
+        public string? Nombres { get; set; }
+
+        public string? Direccion { get; set; }
+
+        public string? RNC { get; set; }
+
+        public decimal LimiteCredito { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+    }
+}
