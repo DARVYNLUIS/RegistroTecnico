5e881f6b85492c3af0564940712f227f3fd8f0cf
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-10-13 20:09:13 -0400

    IClienteService.

---
+++ Clientes.Abstraction/Clientes.Abstraction/IClientesService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Clientes.Data.Models;
+
+namespace ClienteAPI.Abstractions;
+
+public interface IClientesService
+{
+    Task<bool> Guardar(Cliente cliente);
+    Task<bool> Eliminar(int id);
+    Task<Cliente?> Buscar(int id);
+    Task<List<Cliente>> Listar(Expression<Func<Cliente, bool>> criterio);
+}
